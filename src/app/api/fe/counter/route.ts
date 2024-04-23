c7aa150eb615d808bff8320f032d9facc2494eb9
irfanakbari <irfanakbarihabibi18@gmail.com>
2024-04-23 09:30:46 +0700

    fix type

--- src/app/api/fe/counter/route.ts
+++ src/app/api/fe/counter/route.ts
@@ -21,23 +21,23 @@ export async function GET(request: NextRequest) {
     const countVote = await prisma.vote.count()
     // Calculate total votes for each candidate in each nomination
     // Calculate total votes for each candidate in each nomination
-    const nominationsWithTotalVotes = nomination.map((nomination) => {
-        const candidatesWithTotalVotes = nomination.candidates.map((candidate) => {
+    const nominationsWithTotalVotes = nomination.map((nomination:any) => {
+        const candidatesWithTotalVotes = nomination.candidates.map((candidate: any) => {
             const totalVotes = candidate.Vote.length;
             const { Vote, ...candidateWithoutVote } = candidate;
             return { ...candidateWithoutVote, totalVotes };
         });
 
         // Sort candidates within each nomination based on totalVotes in descending order
-        const sortedCandidates = candidatesWithTotalVotes.sort((a, b) => b.totalVotes - a.totalVotes);
+        const sortedCandidates = candidatesWithTotalVotes.sort((a: any, b: any) => b.totalVotes - a.totalVotes);
 
         return { ...nomination, candidates: sortedCandidates };
     });
 
     // Sort nominations based on the highest totalVotes among candidates
     const sortedNominations = nominationsWithTotalVotes.sort((a, b) => {
-        const totalVotesA = a.candidates.reduce((total, candidate) => total + candidate.totalVotes, 0);
-        const totalVotesB = b.candidates.reduce((total, candidate) => total + candidate.totalVotes, 0);
+        const totalVotesA = a.candidates.reduce((total: any, candidate: any) => total + candidate.totalVotes, 0);
+        const totalVotesB = b.candidates.reduce((total: any, candidate: any) => total + candidate.totalVotes, 0);
         return totalVotesB - totalVotesA;
     });
 
